4f4ab9ffadbcb9559503874cd2fe5bd1f918edee
liamwong <liamwong@buffalo.edu>
2019-07-25 18:15:01 -0400

    first commit

---
+++ cmake-build-debug/CMakeFiles/A2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/A2.dir/main.cpp.o"
+  "A2.pdb"
+  "A2"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/A2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
